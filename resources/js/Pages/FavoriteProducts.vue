bd23b0e2b711fa2821d3ad250ef1f3a7dcec3577
vitaliishevchuk <vitaliishevchuk123@gmail.com>
2023-12-06 10:08:27 +0200

    manage favorite filter

--- resources/js/Pages/FavoriteProducts.vue
+++ resources/js/Pages/FavoriteProducts.vue
@@ -5,6 +5,10 @@ import Breadcrumbs from "@/Components/Breadcrumbs.vue";
 import MainTitle from "@/Components/MainTitle.vue";
 import ContentWrapper from "@/Components/ContentWrapper.vue";
 import ProductCard from "@/Components/ProductCard.vue";
+import {computed} from "vue";
+import {useStore} from "vuex";
+
+const store = useStore();
 
 const props = defineProps({
         title: String,
@@ -12,6 +16,12 @@ const props = defineProps({
         favoriteProducts: Array,
     }
 )
+
+const filteredFavoriteProducts = computed(() => {
+    const favoriteIds = store.state.favoriteIds;
+    return props.favoriteProducts.filter(product => favoriteIds.includes(product.id));
+});
+
 </script>
 
 <template>
@@ -21,7 +31,7 @@ const props = defineProps({
             <Breadcrumbs :breadcrumbs="breadcrumbs" class="mb-6"/>
             <MainTitle title="Улюблені"/>
             <div class="product-cards grid grid-cols-4 gap-6 mr-4 mb-6">
-                <ProductCard v-for="(product, index) in favoriteProducts"
+                <ProductCard v-for="(product, index) in filteredFavoriteProducts"
                              :key="'favorite-product-' + index"
                              :product="product"
                 />
